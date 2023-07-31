688e44e075997893dcfd397a282285c52c82c5de
liquidsec <paul.mueller08@gmail.com>
2023-07-31 14:41:06 -0400

    testing initial

--- scripts/readsources.py
+++ scripts/readsources.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print("running!")
+import os
+
+# Define the directory you want to read files from
+directory = './dnsReaper/signatures'
+
+# Get a list of all files in the directory
+files = os.listdir(directory)
+
+# Loop through the files
+for filename in files:
+	print(filename)
+       
