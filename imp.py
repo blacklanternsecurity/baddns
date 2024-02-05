7caa1554a457208e29b50e1ee125fbc4c684cc51
liquidsec <paul.mueller08@gmail.com>
2024-02-05 13:19:29 -0500

    black

--- imp.py
+++ imp.py
@@ -1,5 +1,5 @@
-# This is a temporary workaround to address the incompatibility of the python-whois library with python 3.12. 
-# The issue is with the downstream https://github.com/PythonCharmers/python-future/ library, which is currently not being maintained. 
+# This is a temporary workaround to address the incompatibility of the python-whois library with python 3.12.
+# The issue is with the downstream https://github.com/PythonCharmers/python-future/ library, which is currently not being maintained.
 # This file will short-circuit the problematic import which should prevent breaking compatibility until a real fix is in place in either whois or future.
 
 pass
