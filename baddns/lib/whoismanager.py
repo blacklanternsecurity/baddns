709481708877e0d3da2861a7975fef0121a0bb0c
liquidsec <paul.mueller08@gmail.com>
2024-07-07 19:05:23 -0400

    safer error

--- baddns/lib/whoismanager.py
+++ baddns/lib/whoismanager.py
@@ -25,7 +25,7 @@ class WhoisManager:
             log.debug(f"Got PywhoisError for whois request for {ext.registered_domain}")
             self.whois_result = {"type": "error", "data": str(e)}
         except Exception as e:
-            log.debug(f"Got unknown error from whois: {e}")
+            log.debug(f"Got unknown error from whois: {str(e)}")
             self.whois_result = {"type": "error", "data": str(e)}
 
     def analyzeWHOIS(self):
