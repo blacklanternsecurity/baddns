1d45217a594eaa96625a40f9cfed7335ed55f695
liquidsec <paul.mueller08@gmail.com>
2024-07-10 09:30:36 -0400

    quieting output from whois

--- baddns/lib/whoismanager.py
+++ baddns/lib/whoismanager.py
@@ -18,14 +18,14 @@ class WhoisManager:
         log.debug(f"Extracted base domain [{ext.registered_domain}] from [{self.target}]")
         log.debug(f"Submitting WHOIS query for {ext.registered_domain}")
         try:
-            w = await asyncio.to_thread(whois.whois, ext.registered_domain)
+            w = await asyncio.to_thread(whois.whois, ext.registered_domain, quiet=True)
             log.debug(f"Got response to whois request for {ext.registered_domain}")
             self.whois_result = {"type": "response", "data": w}
         except whois.parser.PywhoisError as e:
             log.debug(f"Got PywhoisError for whois request for {ext.registered_domain}")
             self.whois_result = {"type": "error", "data": str(e)}
         except Exception as e:
-            log.debug(f"Got unknown error from whois: {str(e)}")
+            log.warning(f"Got unknown error from whois: {str(e)}")
             self.whois_result = {"type": "error", "data": str(e)}
 
     def analyzeWHOIS(self):
